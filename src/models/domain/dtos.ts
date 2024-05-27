1c640efa41d6ec3b07141537132d752991ae0812
Otávio Sena <otaviosena.dev@proton.me>
2024-05-27 13:06:12 -0400

    corrigindo paginação de filmes sugeridos

--- src/models/domain/dtos.ts
+++ src/models/domain/dtos.ts
@@ -59,3 +59,31 @@ export interface CreateMovieFavorite {
   watched: boolean
   favorite: boolean
 }
+
+export interface ResponseTmdb {
+  movies: IMovieTmdb[]
+  total_pages: number
+  total_results: number
+}
+
+export interface IMovieTmdb {
+  adult: boolean
+  backdrop_path: string | null
+  genre_ids: number[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string | null
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+export interface IMovieResponse {
+  movies: IMovieTmdb[]
+  total_pages: number
+  total_results: number
+}
--- src/modules/movies/services/SearchMoviesSuggestedService.ts
+++ src/modules/movies/services/SearchMoviesSuggestedService.ts
@@ -2,6 +2,7 @@ import { container, inject, injectable } from 'tsyringe'
 import SearchMoviesSuggested from '../../../shared/tmdb/SearchSuggested'
 import { IMovieRepository } from '../../../models/repositories/IMovieRepository'
 import { IProfileMovieFavorite } from '../../../models/domain/entities'
+import { IMovieResponse, ResponseTmdb } from '../../../models/domain/dtos'
 
 @injectable()
 export default class SearchMoviesSuggestedService {
@@ -10,120 +11,158 @@ export default class SearchMoviesSuggestedService {
     private movieRepository: IMovieRepository,
   ) {}
 
-  public async execute(profile_id: number, page: number): Promise<any> {
+  public async execute(
+    profile_id: number,
+    page: number,
+  ): Promise<ResponseTmdb> {
     const profileMovies = await this.movieRepository.findByProfileId(profile_id)
-    let suggestedMovies: any[] = []
+    let suggestedMovies: ResponseTmdb = {
+      movies: [],
+      total_pages: 0,
+      total_results: 0,
+    }
 
     if (profileMovies.length === 0) {
-      // Se não tiver nenhum filme favorito ainda, busca 20 aleatórios
-      suggestedMovies = await this.requestRandomMovies(page, 20)
+      return await this.requestRandomMovies(page, 20)
     } else {
-      let mainGenre: number | null = null
-      mainGenre = await this.getMainGenre(profileMovies)
-      if (mainGenre !== null) {
-        // Busca 15 filmes do gênero principal e adiciona à lista de filmes sugeridos
-        const mainGenreMovies = await this.requestMoviesByGenre(
-          mainGenre,
-          page,
-          18,
-        )
-        suggestedMovies.push(...mainGenreMovies)
-      }
-
-      // Busca 2 filmes aleatórios que não estão na lista de favoritos
-      const randomMovies = await this.requestRandomMovies(page, 2)
-      const filteredRandomMovies = randomMovies.filter((movie) => {
-        return !profileMovies.some(
-          (profileMovie: any) => profileMovie.tmdbId === movie.id,
-        )
-      })
-      suggestedMovies.push(...filteredRandomMovies)
-
-      // Verifica se há filmes sugeridos duplicados na lista de filmes do perfil
-      let index = 0
-      do {
-        if (index >= suggestedMovies.length) {
-          // Se todos os filmes foram verificados e a lista ainda tem menos de 20 filmes,
-          // busca mais filmes aleatórios para completar a lista
-          const newRandomMovies = await this.requestRandomMovies(
-            page,
-            20 - suggestedMovies.length,
-          )
-          const newFilteredRandomMovies = newRandomMovies.filter((movie) => {
-            return !profileMovies.some(
-              (profileMovie: any) => profileMovie.tmdbId === movie.id,
-            )
-          })
-          suggestedMovies.push(...newFilteredRandomMovies)
-        } else {
-          const movie = suggestedMovies[index]
-          if (
-            profileMovies.some(
-              (profileMovie: any) => profileMovie.tmdbId === movie.id,
-            )
-          ) {
-            // Remove o filme duplicado da lista
-            suggestedMovies.splice(index, 1)
-          } else {
-            index++
-          }
-        }
-      } while (suggestedMovies.length < 20)
+      suggestedMovies = await this.getMoviesBasedOnProfile(profileMovies, page)
     }
 
-    // Adiciona os nomes dos gêneros aos filmes sugeridos
-    for (let i = 0; i < suggestedMovies.length; i++) {
-      const genre = await this.movieRepository.findGenreByTmdbId(
-        suggestedMovies[i].genre_ids[0],
+    suggestedMovies.movies = await this.appendGenreNames(suggestedMovies.movies)
+
+    return {
+      movies: suggestedMovies.movies.slice(0, 20),
+      total_pages: suggestedMovies.total_pages,
+      total_results: suggestedMovies.total_results,
+    }
+  }
+
+  private async getMoviesBasedOnProfile(
+    profileMovies: IProfileMovieFavorite[],
+    page: number,
+  ): Promise<ResponseTmdb> {
+    let suggestedMovies: ResponseTmdb = {
+      movies: [],
+      total_pages: 0,
+      total_results: 0,
+    }
+    const mainGenre = await this.getMainGenre(profileMovies)
+
+    if (mainGenre !== null) {
+      const mainGenreMovies = await this.requestMoviesByGenre(
+        mainGenre,
+        page,
+        20,
       )
-      suggestedMovies[i].genre = genre.name
+      suggestedMovies = mainGenreMovies
     }
 
-    return suggestedMovies.slice(0, 20)
+    return this.getUniqueMovies(suggestedMovies, profileMovies, page)
+  }
+
+  public async getUniqueMovies(
+    suggestedMovies: ResponseTmdb,
+    profileMovies: IProfileMovieFavorite[],
+    page: number,
+  ): Promise<ResponseTmdb> {
+    const profileMovieIds = new Set(profileMovies.map((movie) => movie.tmdbId))
+
+    // Filtre suggestedMovies para remover duplicatas
+    let uniqueSuggestedMovies = suggestedMovies.movies.filter(
+      (movie) => !profileMovieIds.has(movie.id),
+    )
+
+    if (uniqueSuggestedMovies.length === 20) {
+      return {
+        movies: uniqueSuggestedMovies.slice(0, 20),
+        total_pages: suggestedMovies.total_pages,
+        total_results: suggestedMovies.total_results,
+      }
+    } else {
+      while (uniqueSuggestedMovies.length < 20) {
+        page++
+        const moreMovies = await this.requestRandomMovies(
+          page,
+          20 - uniqueSuggestedMovies.length,
+        )
+        const newUniqueMovies = moreMovies.movies.filter(
+          (movie) =>
+            !profileMovieIds.has(movie.id) &&
+            !uniqueSuggestedMovies.some(
+              (uniqueMovie) => uniqueMovie.id === movie.id,
+            ),
+        )
+        uniqueSuggestedMovies = uniqueSuggestedMovies.concat(newUniqueMovies)
+      }
+    }
+    console.log(suggestedMovies)
+    return {
+      movies: uniqueSuggestedMovies.slice(0, 20),
+      total_pages: suggestedMovies.total_pages,
+      total_results: suggestedMovies.total_results,
+    }
+  }
+
+  private async appendGenreNames(suggestedMovies: any[]): Promise<any[]> {
+    return Promise.all(
+      suggestedMovies.map(async (movie) => {
+        const genre = await this.movieRepository.findGenreByTmdbId(
+          movie.genre_ids[0],
+        )
+        movie.genre = genre.name
+        return movie
+      }),
+    )
   }
 
   private async requestRandomMovies(
     page: number,
     limit: number,
-  ): Promise<any[]> {
+  ): Promise<ResponseTmdb> {
     const result = await container
       .resolve(SearchMoviesSuggested)
       .requestToApiWithoutGenres(page)
-    return result.results.slice(0, limit)
+    console.log(result)
+    const movies = result.results.slice(0, limit)
+    const total_pages = result.total_pages
+    const total_results = result.total_results
+
+    return { movies, total_pages, total_results }
   }
 
   private async requestMoviesByGenre(
     genreId: number,
     page: number,
     limit: number,
-  ): Promise<any[]> {
+  ): Promise<ResponseTmdb> {
     const result = await container
       .resolve(SearchMoviesSuggested)
       .requestToApiWithGenres(page, genreId)
 
-    return result.results.slice(0, limit)
-  }
-  private async getMainGenre(movies: IProfileMovieFavorite[]): Promise<number> {
-    // Objeto para armazenar a contagem de cada genre_tmdb_id
-    const countMap: { [key: number]: number } = {}
+    const movies = result.results.slice(0, limit)
+    const total_pages = result.total_pages
+    const total_results = result.total_results
 
-    // Loop pelos filmes e contar ocorrências de cada genre_tmdb_id
+    return { movies, total_pages, total_results }
+  }
+
+  private async getMainGenre(movies: IProfileMovieFavorite[]): Promise<number> {
+    const countMap: { [key: number]: number } = {}
     movies.forEach((movie: any) => {
       const genreId = movie.genre_tmdb_id
       countMap[genreId] = (countMap[genreId] || 0) + 1
     })
 
-    // Variáveis para armazenar o genre_tmdb_id mais frequente e sua contagem
     let mostFrequentGenreId: number | undefined
     let maxCount = 0
 
-    // Loop pelo mapa de contagem para encontrar o genre_tmdb_id mais frequente
     for (const genreId in countMap) {
       if (countMap[genreId] > maxCount) {
         mostFrequentGenreId = parseInt(genreId)
         maxCount = countMap[genreId]
       }
     }
+
     if (mostFrequentGenreId !== undefined) {
       return mostFrequentGenreId
     } else {
